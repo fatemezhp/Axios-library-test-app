0b5c9cc37f418746a13bb5d3274fbe60af508288
Fatemezahra Habibpor <fatemeahbibpour@gmail.com>
2024-07-25 16:38:11 +0330

    updated

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,24 @@
-
-function Layout({children}) {
+function Layout({ children }) {
   return (
     <>
-        <header>header</header>
-        {children}
-        <footer>footer</footer>
+      <header
+        className=" bg-gray-700 flex justify-between my-5
+       rounded-md px-5 py-4 font-bold items-center"
+      >
+        <h1 className="text-md text-">Axios Library Test</h1>
+        <p>React</p>
+      </header>
+
+      {children}
+
+      <footer
+        className=" bg-gray-700 flex justify-around my-5
+       rounded-md px-5 py-4 font-bold items-center"
+      >
+        Developed by Fate
+      </footer>
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -12,14 +12,20 @@ function Posts() {
   console.log(posts);
   return (
     <div>
-      <ul>
-      {posts.map(post => <li key={post.id}>
-        <h4>{post.title}</h4>
-        <p>{post.body}</p>
-      </li>) }
+      <h3 className="border-b-2 font-bold mb-3">Newest Posts</h3>
+      <ul className="grid grid-cols-12 gap-1">
+        {posts.map((post) => (
+          <li
+            className="p-1 rounded bg-gray-700  text-xs grid col-span-4 border w-2/3 h-min text-gray-400"
+            key={post.id}
+          >
+            <h4 className="border-b-2 font-bold">{post.title}</h4>
+            <p>{post.body}</p>
+          </li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
 
 export default Posts;
